5d03d37376fc7ddd191b4d5953ceb25e2bd32abc
michiel.desmet <michiel.desmet@mybrand.nl>
2023-03-28 11:53:49 +0200

    #0 - imported SAP UI 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
 import './App.css';
 import MyCounter from "./components/MyCounter";
+import {ThemeProvider} from "@ui5/webcomponents-react";
+import React, {Component} from "react";
 
-function App() {
-    return (
-        <div className="App">
-            <div>
-                <h2>Default counter</h2>
-                <MyCounter/>
-            </div>
-            <hr/>
-            <div>
-                <h2>Counter with predefined value</h2>
-                <MyCounter value={5}/>
-            </div>
-        </div>
-    );
+class App extends Component {
+
+    render() {
+        return (
+            <ThemeProvider>
+                    <div className="App">
+                        <div>
+                            <h2>Default counter</h2>
+                            <MyCounter/>
+                        </div>
+                        <hr/>
+                        <div>
+                            <h2>Counter with predefined value</h2>
+                            <MyCounter value={5}/>
+                        </div>
+                    </div>
+            </ThemeProvider>
+        );
+    }
+
 }
 
 export default App;
